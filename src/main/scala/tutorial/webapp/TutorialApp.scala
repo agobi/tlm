71908e03bc705f1e2ea4ee44cd7b6185ff71a7d6
Attila Gobi <attila.gobi@gmail.com>
2020-11-21 07:29:39 +0100

    scalajs tutorial #4

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -3,7 +3,14 @@ package tutorial.webapp
 import org.scalajs.dom
 import org.scalajs.dom.document
 
+import scala.scalajs.js.annotation.JSExportTopLevel
+
 object TutorialApp {
+  @JSExportTopLevel("addClickedMessage")
+  def addClickedMessage(): Unit = {
+    appendPar(document.body, "You clicked the button!")
+  }
+
   def appendPar(targetNode: dom.Node, text: String): Unit = {
     val parNode = document.createElement("p")
     parNode.textContent = text
